321203be24372531f64552660d74c13ffd205a25
Heoju <heojh93@naver.com>
2017-11-04 02:51:57 +0900

    3-1

---
+++ practice/CMakeFiles/p31.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kimmj/Documents/hyu/ComputerGraphics/HomeWork/ComputerGraphics/practice3-2/practice3_1.cpp" "/Users/kimmj/Documents/hyu/ComputerGraphics/HomeWork/ComputerGraphics/practice3-2/CMakeFiles/p31.dir/practice3_1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
